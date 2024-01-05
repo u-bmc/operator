253523f0a84e76b2d1a968196d04674c10e8b2d4
Marvin Drees <marvin.drees@9elements.com>
2024-01-05 18:44:04 +0100

    feat: add zerolog and use as default logger
    Signed-off-by: Marvin Drees <marvin.drees@9elements.com>

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -8,8 +8,26 @@ import (
 
 	"github.com/go-logr/logr"
 	"github.com/go-logr/stdr"
+	"github.com/go-logr/zerologr"
+	"github.com/rs/zerolog"
 )
 
-func NewDefaultLogger() logr.Logger {
+const (
+	InfoLevel = iota
+	DebugLevel
+	TraceLevel
+)
+
+func NewDefaultStdLogger() logr.Logger {
 	return stdr.New(stdlog.New(os.Stdout, "", stdlog.LstdFlags))
+}
+
+func NewDefaultLogger() logr.Logger {
+	zl := zerolog.
+		New(zerolog.NewConsoleWriter()).
+		With().
+		Timestamp().
+		Logger()
+
+	return zerologr.New(&zl)
 }
